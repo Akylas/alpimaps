3622fa6508cc33867b24e6aa41c85af7c275f107
Martin Guillon <martin.guillon@akylas.fr>
2021-07-13 18:27:24 +0200

    Revert "chore: not used anymore"
    This reverts commit 77ae545a8f9ff42da0164d59627a6c72b27ac726.

---
+++ webapp/LocalHeightProvider.ts
@@ -0,0 +1,40 @@
+import { MapProvider } from '../geo-three/source/providers/MapProvider';
+import { CancelablePromise } from '../geo-three/source/utils/CancelablePromise';
+import { XHRUtils } from '../geo-three/source/utils/XHRUtils';
+
+export class LocalHeightProvider extends MapProvider {
+    public constructor() {
+        super();
+        this.name = 'local';
+        this.minZoom = 5;
+        this.maxZoom = 12;
+    }
+
+    public async fetchTile(zoom, x, y): Promise<HTMLImageElement> {
+        const result = await Promise.all([
+            new CancelablePromise<HTMLImageElement>((resolve, reject) => {
+                const image = document.createElement('img');
+                image.onload = () => resolve(image);
+                image.onerror = () => resolve(null);
+                image.crossOrigin = 'Anonymous';
+                image.src = `http://127.0.0.1:8080?source=height&x=${x}&y=${y}&z=${zoom}`;
+            })
+        ]);
+        return result[0];
+    }
+
+    public async fetchPeaks(zoom, x, y): Promise<any[]> {
+        return new CancelablePromise((resolve, reject) => {
+            const url = `http://127.0.0.1:8080?source=peaks&x=${x}&y=${y}&z=${zoom}`;
+            try {
+                XHRUtils.get(url, async (data) => {
+                    let result = JSON.parse(data).features;
+                    result = result.filter((f) => f.properties.name);
+                    resolve(result);
+                });
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+}
