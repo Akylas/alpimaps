40d5588ee19c89b289c865d9b0c6d60c7524e9e2
Martin Guillon <martin.guillon@akylas.fr>
2021-08-21 15:49:49 +0200

    big update

--- app/components/OptionPicker.svelte
+++ app/components/OptionPicker.svelte
@@ -7,13 +7,8 @@
 <collectionview items={options} rowHeight="50" height="200">
     <Template let:item>
         <gridlayout columns="*, auto" class="list-group-item" orientation="horizontal" padding="10">
-            <label fontSize="18" text={item.name}  verticalAlignment="center" />
-            <switch
-                col={1}
-                ios:backgroundColor="#f27743"
-                checked={item.checked}
-                on:onCheckedChange={(event) => (item.checked = event.value)}
-                verticalAlignment="center" />
+            <label fontSize="18" text={item.name} verticalAlignment="center" />
+            <switch col={1} ios:backgroundColor="#f27743" checked={item.checked} on:onCheckedChange={(event) => (item.checked = event.value)} verticalAlignment="center" />
         </gridlayout>
     </Template>
 </collectionview>
