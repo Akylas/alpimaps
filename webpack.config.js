70e4edda995354a39211bc085bcd5c98ecd09fe6
Martin Guillon <martin.guillon@akylas.fr>
2021-10-17 21:45:17 +0200

    chore: faster webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,31 @@ const webpack = require('@nativescript/webpack');
 module.exports = (env) => {
     webpack.init(env);
     webpack.chainWebpack((config, env) => {
-        config.module
-            .rule('svelte')
-            .use('string-replace-loader')
-            .loader('string-replace-loader')
-            .options({
-                search: 'createElementNS\\("https:\\/\\/svelte\\.dev\\/docs#svelte_options"',
-                replace: 'createElementNS(svN',
-                flags: 'gm'
-            })
-            .end();
+        // config.module
+        //     .rule('ts')
+        //     .clear()
+        //     .use('esbuild-loader')
+        //     .loader('esbuild-loader')
+        //     .options({
+        //         loader: 'tsx'
+        //     })
+        //     .end();
+        config.when(env.production, (config) => {
+            config.module
+                .rule('svelte')
+                .use('string-replace-loader')
+                .loader('string-replace-loader')
+                .before('svelte-loader')
+                .options({
+                    search: 'createElementNS\\("https:\\/\\/svelte\\.dev\\/docs#svelte_options"',
+                    replace: 'createElementNS(svN',
+                    flags: 'gm'
+                })
+                .end();
+        });
+
         return config;
     });
+    // return webpack.resolveChainableConfig();
     return webpack.resolveConfig();
 };
