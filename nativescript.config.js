415c24ca8bb49b09f4df37d64bc07e04327d2ea6
Martin Guillon <martin.guillon@akylas.fr>
2021-12-31 19:23:58 +0100

    chore: disable threading for now

--- nativescript.config.js
+++ nativescript.config.js
@@ -11,7 +11,7 @@ module.exports = {
         markingMode: 'none',
         // v8Flags: '--expose_gc',
         codeCache: true,
-        enableMultithreadedJavascript: true,
+        enableMultithreadedJavascript: false,
         forceLog: true
     },
     cssParser: 'rework'
