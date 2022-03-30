8ad2cfed288396be47db1d9da64dd989dea04f0d
Martin Guillon <martin.guillon@akylas.fr>
2022-03-30 16:19:34 +0200

    feat: long press on direction params
    to have slider to define value more precisely

---
+++ app/utils/popover.common.ts
@@ -0,0 +1,14 @@
+export enum HorizontalPosition {
+    CENTER,
+    LEFT,
+    RIGHT,
+    ALIGN_LEFT,
+    ALIGN_RIGHT
+}
+export enum VerticalPosition {
+    CENTER,
+    ABOVE,
+    BELOW,
+    ALIGN_TOP,
+    ALIGN_BOTTOM
+}
