52287582a915a61c2ada24fdb9dc82d1896c6c17
Martin Guillon <martin.guillon@akylas.fr>
2022-04-11 16:32:50 +0200

    chore: typings

--- app/utils/utils.d.ts
+++ app/utils/utils.d.ts
@@ -1,1 +1,3 @@
 export * from './utils.android';
+export async function pickTime(currentDate: Dayjs): Promise<[number, number]>;
+export async function pickDate(currentDate: Dayjs): Promise<number>;
