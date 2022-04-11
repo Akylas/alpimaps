d39776b9d52129e797852d4c8527371a485ab8f7
Martin Guillon <martin.guillon@akylas.fr>
2022-04-11 16:32:38 +0200

    chore: refactoring

--- app/utils/utils.android.ts
+++ app/utils/utils.android.ts
@@ -1,6 +1,9 @@
 export * from './utils.common';
 import { sdkVersion } from './utils.common';
-import { Application } from '@nativescript/core';
+import { Application, Frame } from '@nativescript/core';
+import { Dayjs } from 'dayjs';
+import { getRootView } from '@nativescript/core/application';
+import { lc } from '@nativescript-community/l';
 
 export function enableShowWhenLockedAndTurnScreenOn() {
     const activity = Application.android.startActivity as android.app.Activity;
@@ -25,4 +28,40 @@ export function disableShowWhenLockedAndTurnScreenOn() {
             // || android.view.WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
         );
     }
+}
+
+export async function pickDate(currentDate: Dayjs) {
+    return new Promise<number>((resolve, reject) => {
+        const datePicker = com.google.android.material.datepicker.MaterialDatePicker.Builder.datePicker().setTitleText(lc('pick_date')).setSelection(new java.lang.Long(currentDate.valueOf())).build();
+        datePicker.addOnDismissListener(
+            new android.content.DialogInterface.OnDismissListener({
+                onDismiss: () => {
+                    resolve(datePicker.getSelection().longValue());
+                }
+            })
+        );
+        const parentView = Frame.topmost() || getRootView();
+        datePicker.show(parentView._getRootFragmentManager(), 'datepicker');
+    });
+}
+
+export async function pickTime(currentDate: Dayjs) {
+    return new Promise<[number, number]>((resolve, reject) => {
+        const timePicker = new (com.google.android.material as any).timepicker.MaterialTimePicker.Builder()
+            .setTimeFormat(1)
+            .setTitleText(lc('pick_time'))
+            .setHour(currentDate.get('h'))
+            .setMinute(currentDate.get('m'))
+            .build();
+
+        timePicker.addOnDismissListener(
+            new android.content.DialogInterface.OnDismissListener({
+                onDismiss: () => {
+                    resolve([timePicker.getMinute(), timePicker.getHour()]);
+                }
+            })
+        );
+        const parentView = Frame.topmost() || getRootView();
+        timePicker.show(parentView._getRootFragmentManager(), 'timepicker');
+    });
 }
