a29c036d1dbcd29ca4394357e38c3937778622b9
Martin Guillon <martin.guillon@akylas.fr>
2022-05-02 09:36:17 +0200

    chore: trying svelte with esbuild (not working)

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,24 @@
-const sveltePreprocess = require('svelte-preprocess');
-const svelteNativePreprocessor = require('svelte-native-preprocessor');
-const { transformSync } = require('@swc/core');
+const preprocess = require('svelte-preprocess');
+// const svelteNativePreprocessor = require('svelte-native-preprocessor');
+// const { transformSync } = require('@swc/core');
 const { typescript } = require('svelte-preprocess-esbuild');
+// import preprocess from 'svelte-preprocess';
 
 module.exports = {
+    // preprocess: [
+    //     typescript({
+    //         target: 'es2019'
+    //     }),
+    //     preprocess({
+    //       // avoid double
+    //       typescript: false
+    //     })
+    //   ],
     compilerOptions: {
         namespace: 'foreign'
     },
     preprocess: [
-        // typescript({
-        //     target: 'es2019'
-        // }),
-        // sveltePreprocess({
-        //     typescript: false
-        // })
-        sveltePreprocess({
+        preprocess({
             typescript: {
                 compilerOptions: {
                     target: 'es2019'
