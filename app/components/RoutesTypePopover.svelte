e3dbcd838cc2086e00ddb923de2551c57e1590de
farfromrefuge <dev@akylas.fr>
2023-05-14 21:24:13 +0200

    chore: routeType popover

--- app/components/RoutesTypePopover.svelte
+++ app/components/RoutesTypePopover.svelte
@@ -2,16 +2,17 @@
     import { RoutesType } from '~/mapModules/CustomLayersModule';
     import { lc } from '@nativescript-community/l';
     import { routesType } from '~/stores/mapStore';
+    import { widgetBackgroundColor } from '~/variables';
 </script>
 
 <script lang="ts">
+    export let backgroundColor = $widgetBackgroundColor;
 </script>
 
-<gridLayout>
-    <stacklayout>
-        <checkbox text={lc('all')} checked={$routesType === RoutesType.All} on:checkedChange={(e) => routesType.set(RoutesType.All)} />
-        <checkbox text={lc('bicycle')} checked={$routesType === RoutesType.Bicycle} on:checkedChange={(e) => routesType.set(RoutesType.Bicycle)} />
-        <checkbox text={lc('hiking')} checked={$routesType === RoutesType.Hiking} on:checkedChange={(e) => routesType.set(RoutesType.Hiking)} />
-
-    </stacklayout>
-</gridLayout>
+<gesturerootview columns="auto">
+    <stacklayout borderRadius={4} {backgroundColor} margin={2} elevation={2} width="auto" horizontalAlignment="left" padding="10">
+    <checkbox boxType="circle" text={lc('all')} checked={$routesType === RoutesType.All} on:tap={(e) => routesType.set(RoutesType.All)} />
+    <checkbox boxType="circle" text={lc('bicycle')} checked={$routesType === RoutesType.Bicycle} on:tap={(e) => routesType.set(RoutesType.Bicycle)} />
+    <checkbox boxType="circle" text={lc('hiking')} checked={$routesType === RoutesType.Hiking} on:tap={(e) => routesType.set(RoutesType.Hiking)} />
+</stacklayout>
+</gesturerootview>
