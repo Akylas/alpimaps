9f5609d80e13710a8f92e38e46823919001cd748
farfromrefuge <dev@akylas.fr>
2023-07-06 16:27:49 +0200

    feat: new edit marker / route window. For now accessible from the items list window

---
+++ app/components/ItemEdit.svelte
@@ -0,0 +1,321 @@
+<script lang="ts">
+    import { MapBounds } from '@nativescript-community/ui-carto/core';
+    import { GeoJSONVectorTileDataSource } from '@nativescript-community/ui-carto/datasources';
+    import { VectorTileLayer, VectorTileRenderOrder } from '@nativescript-community/ui-carto/layers/vector';
+    import { CartoMap, PanningMode } from '@nativescript-community/ui-carto/ui';
+    import { Color, Utils } from '@nativescript/core';
+    import type { Point as GeoJSONPoint } from 'geojson';
+    import { goBack, showModal } from 'svelte-native/dom';
+    import { osmicon } from '~/helpers/formatter';
+    import { lc } from '~/helpers/locale';
+    import { formatter } from '~/mapModules/ItemFormatter';
+    import { getMapContext } from '~/mapModules/MapModule';
+    import { Item } from '~/models/Item';
+    import { showError } from '~/utils/error';
+    import { pickColor } from '~/utils/utils';
+    import { lightBackgroundColor, mdiFontFamily, navigationBarHeight, subtitleColor, textLightColor } from '~/variables';
+    import CActionBar from './CActionBar.svelte';
+
+    export let item: Item;
+    let itemColor: Color;
+    let itemIsRoute = false;
+    let itemUsingDefault = false;
+    let itemIcon: string = null;
+    let itemIconFontFamily: string = null;
+    let itemUsingMdi = false;
+    let osmIcon = null;
+    $: itemIsRoute = !!item?.route;
+    $: itemColor = updatedProperties['color'] || item.properties['color'] || (itemIsRoute ? '#287bda' : '#60A5F4');
+    $: itemIcon = updatedProperties['icon'] || item.properties['icon'];
+    $: osmIcon = osmicon(formatter.geItemIcon(item));
+    $: itemIconFontFamily = updatedProperties['fontFamily'] || item.properties['fontFamily'];
+    $: itemUsingMdi = itemIconFontFamily === mdiFontFamily;
+    $: itemUsingDefault = itemUsingMdi && itemIcon === 'mdi-map-marker';
+    $: itemUsingOsm = itemIcon === osmIcon && itemIconFontFamily === 'osm';
+    let updatedProperties = {};
+    const mapContext = getMapContext();
+    function blurTextField() {
+        Utils.dismissSoftInput();
+    }
+    let updatingItem = false;
+    async function updateItem() {
+        try {
+            updatingItem = true;
+            const savedItem = await mapContext.mapModule('items').updateItem(item, { properties: { ...item.properties, ...updatedProperties } });
+            if (mapContext.getSelectedItem()?.id === item.id) {
+                mapContext.setSelectedItem(savedItem);
+            }
+            goBack();
+        } catch (err) {
+            showError(err);
+        } finally {
+            updatingItem = false;
+        }
+    }
+    function onTextChange(property, e) {
+        updatedProperties[property] = e['value'];
+        updatePreview();
+    }
+    let cartoMap: CartoMap<LatLonKeys>;
+    let vectorTileDataSource: GeoJSONVectorTileDataSource;
+    let vectorTileLayer: VectorTileLayer;
+
+    function updatePreview() {
+        updatedProperties = updatedProperties;
+        const str = JSON.stringify({
+            type: 'FeatureCollection',
+            features: [{ type: 'Feature', id: item.id, properties: Object.assign({}, item.properties, updatedProperties), geometry: item.geometry }]
+        });
+        // DEV_LOG && console.log('updateGeoJSONLayer', str);
+        vectorTileDataSource.setLayerGeoJSONString(1, str);
+    }
+    async function onMapReady(e) {
+        cartoMap = e.object as CartoMap<LatLonKeys>;
+        // projection = cartoMap.projection;
+        // if (__ANDROID__) {
+        //     console.log('onMapReady', com.carto.ui.BaseMapView.getSDKVersion());
+        // } else {
+        //     console.log('onMapReady', cartoMap.nativeViewProtected as NTMapView);
+        // }
+
+        const options = cartoMap.getOptions();
+        options.setWatermarkScale(0);
+        options.setRestrictedPanning(true);
+        options.setPanningMode(PanningMode.PANNING_MODE_STICKY_FINAL);
+
+        options.setZoomGestures(true);
+        options.setKineticRotation(false);
+        // const route = dataItems.map(i=>([]))
+        try {
+            let layers = mapContext.getLayers('map');
+            if (layers.length === 0) {
+                layers = mapContext.getLayers('customLayers');
+            }
+            layers.forEach((l) => {
+                var prototype = Object.getPrototypeOf(l.layer);
+                cartoMap.addLayer(new prototype.constructor(l.layer.options));
+            });
+
+            vectorTileDataSource = new GeoJSONVectorTileDataSource({
+                simplifyTolerance: 2,
+                minZoom: 0,
+                maxZoom: 24
+            });
+            vectorTileDataSource.createLayer('items');
+            updatePreview();
+            vectorTileLayer = new VectorTileLayer({
+                labelBlendingSpeed: 0,
+                layerBlendingSpeed: 0,
+                labelRenderOrder: VectorTileRenderOrder.LAST,
+                clickRadius: 6,
+                dataSource: vectorTileDataSource,
+                decoder: getMapContext().innerDecoder
+            });
+            vectorTileLayer.setVectorTileEventListener(this);
+            cartoMap.addLayer(vectorTileLayer);
+
+            if (cartoMap.getMeasuredWidth()) {
+                onLayoutChanged();
+            }
+        } catch (err) {
+            showError(err);
+        }
+    }
+    function onLayoutChanged() {
+        if (!cartoMap) {
+            return;
+        }
+        if (itemIsRoute) {
+            const margin = Utils.layout.toDevicePixels(20);
+            const screenBounds = {
+                min: { x: margin, y: margin },
+                max: { x: cartoMap.getMeasuredWidth() - margin, y: cartoMap.getMeasuredHeight() - margin }
+            };
+            if (item.properties?.zoomBounds) {
+                cartoMap.moveToFitBounds(item.properties.zoomBounds, screenBounds, false, true, false, 0);
+            } else if (item.properties?.extent) {
+                let extent: [number, number, number, number] = item.properties.extent as any;
+                if (typeof extent === 'string') {
+                    if (extent[0] !== '[') {
+                        extent = `[${extent}]` as any;
+                    }
+                    extent = JSON.parse(extent as any);
+                }
+                cartoMap.moveToFitBounds(new MapBounds({ lat: extent[1], lon: extent[0] }, { lat: extent[3], lon: extent[2] }), screenBounds, true, true, false, 0);
+            }
+        } else {
+            cartoMap.setZoom(14, 0);
+            const geometry = item.geometry as GeoJSONPoint;
+            const position = { lat: geometry.coordinates[1], lon: geometry.coordinates[0] };
+            cartoMap.setFocusPos(position, 0);
+        }
+    }
+    async function pickOptionColor(color: Color) {
+        try {
+            const newColor = await pickColor(color, { alpha: false });
+            if (!newColor) {
+                return;
+            }
+            updatedProperties['color'] = newColor.hex;
+            updatePreview();
+        } catch (err) {
+            showError(err);
+        }
+    }
+    function setOSMIcon() {
+        updatedProperties['fontFamily'] = 'osm';
+        updatedProperties['mapFontFamily'] = 'osm';
+        updatedProperties['iconDx'] = 0;
+        updatedProperties['icon'] = osmIcon;
+        updatePreview();
+    }
+    function setDefaultIcon() {
+        updatedProperties['fontFamily'] = mdiFontFamily;
+        updatedProperties['mapFontFamily'] = MATERIAL_MAP_FONT_FAMILY;
+        updatedProperties['iconDx'] = -2;
+        updatedProperties['icon'] = 'mdi-map-marker';
+        updatePreview();
+    }
+
+    async function selectCustomIcon() {
+        try {
+            const IconChooser = (await import('~/components/IconChooser.svelte')).default as any;
+            const results = await showModal({
+                fullscreen: true,
+                page: IconChooser as any
+            });
+            const result = Array.isArray(results) ? results[0] : results;
+            if (result) {
+                console.log('result', result);
+                updatedProperties['fontFamily'] = result.fontFamily;
+                itemUsingMdi = result.fontFamily === mdiFontFamily;
+                updatedProperties['mapFontFamily'] = itemUsingMdi ? MATERIAL_MAP_FONT_FAMILY : 'osm';
+                updatedProperties['iconDx'] = itemUsingMdi ? -2 : 0;
+                updatedProperties['icon'] = result.icon;
+                updatePreview();
+                // const provider = result.data;
+                // if (result.isPick) {
+                //     provider.name = File.fromPath(provider.url).name;
+                //     provider.id = provider.url;
+                //     provider.type = 'orux';
+                // }
+                // const data = await this.createRasterLayer(provider.id || result.name, provider);
+                // this.addDataSource(data);
+            }
+        } catch (error) {
+            showError(error);
+        }
+    }
+</script>
+
+<page actionBarHidden={true}>
+    <gridlayout rows="auto,*,3*,auto" android:paddingBottom={$navigationBarHeight}>
+        <CActionBar canGoBack title={lc('edit')} />
+        <cartomap row={1} zoom={16} on:mapReady={onMapReady} useTextureView={false} on:layoutChanged={onLayoutChanged} />
+        <canvaslabel row={1} fontSize={16} height={50} width={50} on:tap={pickOptionColor(itemColor)} horizontalAlignment="right" verticalAlignment="bottom">
+            <circle strokeWidth={2} paintStyle="fill" fillColor={$textLightColor} radius={15} antiAlias={true} horizontalAlignment="right" verticalAlignment="middle" width={20} />
+            <circle
+                strokeWidth={2}
+                paintStyle="fill_and_stroke"
+                strokeColor={$textLightColor}
+                fillColor={itemColor}
+                radius={15}
+                antiAlias={true}
+                horizontalAlignment="right"
+                verticalAlignment="middle"
+                width={20}
+            />
+        </canvaslabel>
+        {#if !itemIsRoute}
+            <gridlayout row={1} columns="auto,auto,auto" height={50} horizontalAlignment="left" verticalAlignment="bottom" margin={5}>
+                <label
+                    visibility={itemUsingDefault ? 'collapse' : 'visible'}
+                    on:tap={setDefaultIcon}
+                    fontSize={22}
+                    borderRadius={4}
+                    backgroundColor={$lightBackgroundColor}
+                    col={0}
+                    verticalAlignment="middle"
+                    textAlignment="center"
+                    padding={5}
+                    width={50}
+                    height={50}
+                    elevation={2}
+                    margin="0 4 0 4"
+                    borderColor={$subtitleColor}
+                    borderWidth={1}
+                    rippleColor={itemColor}
+                >
+                    <span text="mdi-map-marker" fontFamily={mdiFontFamily} color={itemColor} />
+                    <span fontSize={12} text={'\n' + lc('marker')} />
+                </label>
+                <label
+                    visibility={itemUsingOsm ? 'collapse' : 'visible'}
+                    fontSize={20}
+                    on:tap={setOSMIcon}
+                    borderRadius={4}
+                    backgroundColor={$lightBackgroundColor}
+                    col={1}
+                    verticalAlignment="middle"
+                    textAlignment="center"
+                    padding={5}
+                    width={50}
+                    height={50}
+                    elevation={2}
+                    margin="0 4 0 4"
+                    borderColor={$subtitleColor}
+                    borderWidth={1}
+                    rippleColor={itemColor}
+                >
+                    <span text={osmIcon} fontFamily="osm" color={itemColor} />
+                    <span fontSize={12} text={'\n' + lc('osm')} />
+                </label>
+                <label
+                    on:tap={selectCustomIcon}
+                    fontSize={itemUsingMdi ? 22 : 18}
+                    borderRadius={4}
+                    backgroundColor={$lightBackgroundColor}
+                    col={2}
+                    verticalAlignment="middle"
+                    textAlignment="center"
+                    padding={5}
+                    width={50}
+                    height={50}
+                    margin="0 4 0 4"
+                    borderColor={$subtitleColor}
+                    borderWidth={1}
+                    elevation={2}
+                    rippleColor={itemColor}
+                >
+                    <span text={itemIcon} fontFamily={itemIconFontFamily} color={itemColor} />
+                    <span fontSize={12} text={'\n' + lc('custom')} />
+                </label>
+            </gridlayout>
+        {/if}
+        <gridlayout row={2} padding="0 10 0 10" rows="auto,auto,auto,*">
+            <textfield
+                variant="outline"
+                margin="10 0 0 0"
+                hint={lc('name')}
+                placeholder={lc('name')}
+                returnKeyType="done"
+                on:return={blurTextField}
+                text={formatter.getItemTitle(item)}
+                on:textChange={(e) => onTextChange('name', e)}
+            />
+
+            <textview
+                row={3}
+                variant="outline"
+                margin="10 0 0 0"
+                hint={lc('notes')}
+                placeholder={lc('notes')}
+                returnKeyType="done"
+                on:return={blurTextField}
+                text={item.properties['notes']}
+                on:textChange={(e) => onTextChange('notes', e)}
+            />
+        </gridlayout>
+        <mdbutton row={3} on:tap={(e) => updateItem()} text={lc('save')} marginBottom={20} />
+    </gridlayout>
+</page>
--- app/components/ItemsList.svelte
+++ app/components/ItemsList.svelte
@@ -5,47 +5,57 @@
     import { ObservableArray } from '@nativescript/core';
     import SqlQuery from 'kiss-orm/dist/Queries/SqlQuery';
     import { Template } from 'svelte-native/components';
-    import { NativeViewElementNode, goBack } from 'svelte-native/dom';
+    import { NativeViewElementNode, goBack, navigate } from 'svelte-native/dom';
     import { GeoHandler } from '~/handlers/GeoHandler';
-    import { lc, onLanguageChanged } from '~/helpers/locale';
+    import { lc, lu, onLanguageChanged } from '~/helpers/locale';
     import { onThemeChanged } from '~/helpers/theme';
     import { getMapContext } from '~/mapModules/MapModule';
     import { Item } from '~/models/Item';
     import { onServiceLoaded } from '~/services/BgService.common';
     import { showError } from '~/utils/error';
-    import { backgroundColor, borderColor, primaryColor, subtitleColor, textColor } from '~/variables';
+    import { actionBarButtonHeight, actionBarHeight, backgroundColor, borderColor, navigationBarHeight, primaryColor, subtitleColor, textColor } from '~/variables';
     import BottomSheetInfoView from './BottomSheetInfoView.svelte';
     import CActionBar from './CActionBar.svelte';
     import IconButton from './IconButton.svelte';
 
     let collectionView: NativeViewElementNode<CollectionView>;
-    type RouteItem = Item;
-    let items: ObservableArray<RouteItem>;
+    let items: ObservableArray<Item>;
+    let itemsCount = 0;
+    let tabIndex = 0;
     const itemsModule = getMapContext().mapModule('items');
     onServiceLoaded(refresh);
 
     async function refresh() {
-        const sqlItems = await itemsModule.itemRepository.searchItem(SqlQuery.createFromTemplateString`"route" IS NOT NULL`);
+        const sqlItems = await itemsModule.itemRepository.searchItem(tabIndex === 1 ? SqlQuery.createFromTemplateString`"route" IS NULL` : SqlQuery.createFromTemplateString`"route" IS NOT NULL`);
         items = new ObservableArray(sqlItems);
+        itemsCount = items.length;
     }
 
-    function itemIsRoute(item: RouteItem) {
+    function setTabIndex(value: number) {
+        if (tabIndex !== value) {
+            tabIndex = value;
+            refresh();
+        }
+    }
+
+    function itemIsRoute(item: Item) {
         return !!item?.route;
     }
 
-    function deleteItem(item: RouteItem) {
+    function deleteItem(item: Item) {
         try {
             const index = items.indexOf(item);
             itemsModule.deleteItem(item);
             if (index !== -1) {
                 items.splice(index, 1);
             }
+            itemsCount = items.length;
         } catch (error) {
             showError(error);
         }
     }
 
-    function showItemOnMap(item: RouteItem) {
+    function showItemOnMap(item: Item) {
         if (item.onMap) {
             itemsModule.hideItem(item);
         } else {
@@ -57,10 +67,10 @@
             items.setItem(index, item);
         }
         // setMenuVisible(item, false);
-        showSnack({ message: item.onMap ? lc('route_now_visible') : lc('route_now_hidden') });
+        showSnack({ message: item.onMap ? lc('item_now_visible') : lc('item_now_hidden') });
     }
 
-    function startEditingItem(item: RouteItem) {
+    function startEditingItem(item: Item) {
         getMapContext().startEditingItem(item);
         goBack();
     }
@@ -89,23 +99,38 @@
 
         return result;
     }
+
+    async function showDetails(item) {
+        try {
+            const RoutesList = (await import('~/components/ItemEdit.svelte')).default as any;
+            navigate({ page: RoutesList, props: { item } });
+        } catch (error) {
+            showError(error);
+        }
+    }
 </script>
 
 <page actionBarHidden={true}>
     <gridlayout rows="auto,*">
-        <CActionBar canGoBack title={lc('routes')} />
-        <collectionview bind:this={collectionView} row={1} {items} rowHeight={80}>
-            <Template let:item>
+        <CActionBar canGoBack title={lc('items')}>
+            <gridlayout slot="bottom" columns="*,*" height={48}>
+                <label text={lu('routes')} textAlignment="center" verticalTextAlignment="center" fontWeight="500" color="white" on:tap={() => setTabIndex(0)} rippleColor="white" />
+                <label text={lu('markers')} col={1} textAlignment="center" verticalTextAlignment="center" fontWeight="500" color="white" on:tap={() => setTabIndex(1)} rippleColor="white" />
+                <absolutelayout colSpan={2} width="50%" height={3} backgroundColor="white" verticalAlignment="bottom" horizontalAlignment={tabIndex === 1 ? 'right' : 'left'} />
+            </gridlayout>
+        </CActionBar>
+        <collectionview bind:this={collectionView} row={1} {items} rowHeight={80} itemTemplateSelector={(item) => (!!item.route ? 'route' : 'default')} android:paddingBottom={$navigationBarHeight}>
+            <Template let:item key="route">
                 <swipemenu
                     id={item.name}
-                    leftSwipeDistance={200}
-                    rightSwipeDistance={200}
+                    leftSwipeDistance={0}
+                    rightSwipeDistance={0}
                     startingSide={item.startingSide}
                     translationFunction={drawerTranslationFunction}
                     openAnimationDuration={100}
                     closeAnimationDuration={100}
                 >
-                    <gridlayout prop:mainContent backgroundColor={$backgroundColor}>
+                    <gridlayout prop:mainContent backgroundColor={$backgroundColor} on:tap={() => showDetails(item)}>
                         <BottomSheetInfoView {item} marginLeft={60} propsLeft={60} iconLeft={17} iconTop={64} iconColor={$backgroundColor} {onDraw} iconSize={16}>
                             <image noCache={true} src={item.image_path} borderRadius={8} width={50} height={50} horizontalAlignment="left" verticalAlignment="top" marginTop={10} />
                         </BottomSheetInfoView>
@@ -114,21 +139,40 @@
                         <IconButton on:tap={() => deleteItem(item)} tooltip={lc('delete')} shape="none" width={60} height="100%" color="white" backgroundColor="red" text="mdi-trash-can" />
                     </stacklayout>
                     <stacklayout prop:rightDrawer orientation="horizontal">
+                        <IconButton on:tap={() => startEditingItem(item)} tooltip={lc('edit')} text={'mdi-pencil'} shape="none" width={60} height="100%" color="white" backgroundColor={primaryColor} />
                         <IconButton
-                            on:tap={() => startEditingItem(item)}
-                            tooltip={lc('edit')}
-                            isVisible={itemIsRoute(item)}
-                            text={'mdi-pencil'}
+                            on:tap={() => showItemOnMap(item)}
+                            tooltip={lc('show')}
+                            text={!item.onMap ? 'mdi-eye-off' : 'mdi-eye'}
                             shape="none"
                             width={60}
                             height="100%"
                             color="white"
-                            backgroundColor={primaryColor}
+                            backgroundColor={!item.onMap ? 'gray' : 'blue'}
                         />
+                    </stacklayout>
+                </swipemenu>
+            </Template>
+            <Template let:item>
+                <swipemenu
+                    id={item.name}
+                    leftSwipeDistance={0}
+                    rightSwipeDistance={0}
+                    startingSide={item.startingSide}
+                    translationFunction={drawerTranslationFunction}
+                    openAnimationDuration={100}
+                    closeAnimationDuration={100}
+                >
+                    <gridlayout prop:mainContent backgroundColor={$backgroundColor} on:tap={() => showDetails(item)}>
+                        <BottomSheetInfoView {item} />
+                    </gridlayout>
+                    <stacklayout prop:leftDrawer orientation="horizontal" height="100%">
+                        <IconButton on:tap={() => deleteItem(item)} tooltip={lc('delete')} shape="none" width={60} height="100%" color="white" backgroundColor="red" text="mdi-trash-can" />
+                    </stacklayout>
+                    <stacklayout prop:rightDrawer orientation="horizontal">
                         <IconButton
                             on:tap={() => showItemOnMap(item)}
                             tooltip={lc('show')}
-                            isVisible={itemIsRoute(item)}
                             text={!item.onMap ? 'mdi-eye-off' : 'mdi-eye'}
                             shape="none"
                             width={60}
@@ -140,6 +184,13 @@
                 </swipemenu>
             </Template>
         </collectionview>
-        <label row={1} text={lc('no_route')} color={$subtitleColor} visibility={items && items.length ? 'hidden' : 'visible'} textAlignment="center" verticalAlignment="middle" />
+        <label
+            row={1}
+            text={tabIndex === 1 ? lc('no_marker') : lc('no_route')}
+            color={$subtitleColor}
+            visibility={itemsCount ? 'hidden' : 'visible'}
+            textAlignment="center"
+            verticalAlignment="middle"
+        />
     </gridlayout>
 </page>
