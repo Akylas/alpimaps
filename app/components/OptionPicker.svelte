4378cddc1e993ede6f0febd32f6387440a4030c7
farfromrefuge <dev@akylas.fr>
2023-07-06 16:40:36 +0200

    chore: refactor

--- app/components/OptionPicker.svelte
+++ app/components/OptionPicker.svelte
@@ -8,7 +8,7 @@
     <Template let:item>
         <gridlayout columns="*, auto" class="list-group-item" orientation="horizontal" padding={10}>
             <label fontSize={18} text={item.name} verticalAlignment="middle" />
-            <switch col={1} ios:backgroundColor="#f27743" checked={item.checked} on:onCheckedChange={(event) => (item.checked = event.value)} verticalAlignment="middle" />
+            <switch col={1} checked={item.checked} on:checkedChange={(event) => (item.checked = event.value)} verticalAlignment="middle" ios:backgroundColor="#f27743" />
         </gridlayout>
     </Template>
 </collectionview>
