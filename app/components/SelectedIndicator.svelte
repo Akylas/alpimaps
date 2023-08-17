574c260ef94ea76fd25b65fa6e9cbb3d5a082f85
farfromrefuge <dev@akylas.fr>
2023-08-17 15:00:07 +0200

    fix: improved selected indicator for lists

--- app/components/SelectedIndicator.svelte
+++ app/components/SelectedIndicator.svelte
@@ -1,22 +1,26 @@
-<script lang="ts">
-    import { primaryColor } from '~/variables';
-    export let selected: boolean = false;
+<script lang="ts" context="module">
+    import { Align, Canvas, CanvasView, Paint } from '@nativescript-community/ui-canvas';
+    import { mdiFontFamily, primaryColor } from '~/variables';
+    const paint = new Paint();
+    paint.color = primaryColor;
+    const iconPaint = new Paint();
+    iconPaint.fontFamily = mdiFontFamily;
+    iconPaint.color = 'white';
+    iconPaint.textSize = 16;
+    iconPaint.setTextAlign(Align.CENTER);
+    const radius = 10;
+    const text = 'mdi-check';
 </script>
 
-<label
-    class="mdi"
-    backgroundColor={primaryColor}
-    color="white"
-    text="mdi-check"
-    fontSize={16}
-    width={20}
-    height={20}
-    borderRadius={10}
-    textAlignment="center"
-    verticalTextAlignment="center"
-    verticalAlignment="top"
-    horizontalAlignment="left"
-    margin={10}
-    visibility={selected ? 'visible' : 'hidden'}
-    {...$$restProps}
-/>
+<script lang="ts">
+    export let selected: boolean = false;
+
+    function onCanvasDraw({ canvas, object }: { canvas: Canvas; object: CanvasView }) {
+        let w2 = canvas.getWidth() / 2;
+        canvas.drawCircle(radius, radius, radius, paint);
+        canvas.drawText(text, radius, radius+ 6, iconPaint);
+    }
+    
+</script>
+
+<canvas disableCss={true} on:draw={onCanvasDraw} width={40} height={40} verticalAlignment="top" horizontalAlignment="left" visibility={selected ? 'visible' : 'hidden'} {...$$restProps}/>
