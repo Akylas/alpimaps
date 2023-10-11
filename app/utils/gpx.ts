58489a905d81afd5bf93c989c30bc34eae6420a5
farfromrefuge <dev@akylas.fr>
2023-10-11 21:42:23 +0200

    feat: share item as gpx

--- app/utils/gpx.ts
+++ app/utils/gpx.ts
@@ -6,7 +6,7 @@ import { Feature, LineString } from 'geojson';
 import { DOMParser, XMLSerializer } from 'xmldom';
 import { getBoundsAndDistance } from '~/helpers/geolib';
 import { IItem } from '~/models/Item';
-global.XMLSerializer = XMLSerializer;
+import { toXML } from 'jstoxml';
 
 export function importGPXToGeojson(filePath: string) {
     const str = File.fromPath(path.join(knownFolders.currentApp().path, 'assets', 'gr52.gpx')).readTextSync();
@@ -37,4 +37,43 @@ export function importGPXToGeojson(filePath: string) {
             return itemFeature as IItem;
         }
     });
+}
+
+function json2xml(o) {
+    const tab = '\t';
+    const toXml = function (v, name, ind) {
+        let xml = '';
+        if (v instanceof Array) {
+            for (let i = 0, n = v.length; i < n; i++) xml += ind + toXml(v[i], name, ind + '\t') + '\n';
+        } else if (typeof v == 'object') {
+            let hasChild = false;
+            xml += ind + '<' + name;
+            for (const m in v) {
+                if (m.charAt(0) === '@') {
+                    xml += ' ' + m.substr(1) + '="' + v[m].toString() + '"';
+                } else {
+                    hasChild = true;
+                }
+            }
+            xml += hasChild ? '>' : '/>';
+            if (hasChild) {
+                for (const m in v) {
+                    if (m === '#text') {
+                        xml += v[m];
+                    } else if (m === '#cdata') {
+                        xml += '<![CDATA[' + v[m] + ']]>';
+                    } else if (m.charAt(0) !== '@') {
+                        xml += toXml(v[m], m, ind + '\t');
+                    }
+                }
+                xml += (xml.charAt(xml.length - 1) === '\n' ? ind : '') + '</' + name + '>';
+            }
+        } else {
+            xml += ind + '<' + name + '>' + v.toString() + '</' + name + '>';
+        }
+        return xml;
+    };
+}
+export function JSONtoXML(json) {
+    return toXML(json);
 }
