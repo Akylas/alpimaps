d2eb374b5dcdec577491758f57906c365dcab266
farfromrefuge <dev@akylas.fr>
2023-10-24 15:03:15 +0200

    feat: import/export settings

--- app/utils/utils.d.ts
+++ app/utils/utils.d.ts
@@ -2,3 +2,4 @@ export * from './utils.android';
 export async function pickTime(currentDate: Dayjs): Promise<[number, number]>;
 export async function pickDate(currentDate: Dayjs): Promise<number>;
 export function moveFileOrFolder(sourceLocationPath: string, targetLocationPath: string, androidTargetLocationPath?: string);
+export function restartApp();
