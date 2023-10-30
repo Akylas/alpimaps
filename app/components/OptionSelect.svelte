26717414d950d8ab587e1a430a02e9b6381db89b
farfromrefuge <dev@akylas.fr>
2023-10-30 09:11:18 +0100

    chore: refactor

--- app/components/OptionSelect.svelte
+++ app/components/OptionSelect.svelte
@@ -20,11 +20,13 @@
     export let showBorders = true;
     export let backgroundColor = $defaultBackgroundColor;
     export let rowHeight = 72;
-    export let width = '*';
+    export let width: string | number = '*';
     export let fontWeight = 'bold';
     export let options: OptionType[];
     export let onClose = null;
-    export let height: number = 350;
+    export let height: number | string = 350;
+    export let fontSize = 16;
+    export let iconFontSize = 16;
     export let onCheckBox: (item, value) => void = null;
     let filteredOptions: OptionType[] = null;
     let filter: string = null;
@@ -73,46 +75,48 @@
     }
 </script>
 
-<gesturerootview {backgroundColor} columns={`${width}`} {height} rows="auto,*" {...$$restProps}>
-    {#if showFilter}
-        <gridlayout borderColor={$borderColor} margin="10 10 0 10">
-            <textfield
-                autocapitalizationType="none"
-                backgroundColor="transparent"
-                height={actionBarButtonHeight}
-                hint={lc('search')}
-                padding="0 30 0 20"
-                placeholder={lc('search')}
-                returnKeyType="search"
-                text={filter}
-                variant="outline"
-                verticalTextAlignment="center"
-                on:returnPress={blurTextField}
-                on:textChange={(e) => (filter = e['value'])} />
+<gesturerootview columns="auto" rows="auto">
+    <gridlayout {backgroundColor} columns={`${width}`} {height} rows="auto,*" {...$$restProps}>
+        {#if showFilter}
+            <gridlayout borderColor={$borderColor} margin="10 10 0 10">
+                <textfield
+                    autocapitalizationType="none"
+                    backgroundColor="transparent"
+                    height={actionBarButtonHeight}
+                    hint={lc('search')}
+                    padding="0 30 0 20"
+                    placeholder={lc('search')}
+                    returnKeyType="search"
+                    text={filter}
+                    variant="outline"
+                    verticalTextAlignment="center"
+                    on:returnPress={blurTextField}
+                    on:textChange={(e) => (filter = e['value'])} />
 
-            <IconButton
-                col={1}
-                gray={true}
-                horizontalAlignment="right"
-                isHidden={!filter || filter.length === 0}
-                size={40}
-                text="mdi-close"
-                verticalAlignment="middle"
-                on:tap={() => (filter = null)} />
-        </gridlayout>
-    {/if}
-    <collectionView itemTemplateSelector={(item) => item.type || 'default'} items={filteredOptions} row={1} {rowHeight}>
-        <Template key="checkbox" let:item>
-            <checkbox checked={item.value} text={item.name} on:checkedChange={(e) => onCheckBox(item, e.value)} />
-        </Template>
-        <Template let:item>
-            <canvaslabel color={item.color || $textColor} padding={16} rippleColor={item.color || $textColor} on:tap={(event) => onTap(item, event)}>
-                <cspan fontFamily={mdiFontFamily} fontSize={16} text={item.icon} textAlignment="left" verticalAlignment="middle" visibility={item.icon ? 'visible' : 'hidden'} />
-                <cspan fontSize={16} {fontWeight} paddingLeft={item.icon ? 30 : 0} text={item.name} textAlignment="left" verticalAlignment="middle" />
-                {#if showBorders}
-                    <line color={$borderColor} height={1} startX={0} startY={0} stopX="100%" stopY={0} strokeWidth={1} verticalAlignment="bottom" />
-                {/if}
-            </canvaslabel>
-        </Template>
-    </collectionView>
+                <IconButton
+                    col={1}
+                    gray={true}
+                    horizontalAlignment="right"
+                    isHidden={!filter || filter.length === 0}
+                    size={40}
+                    text="mdi-close"
+                    verticalAlignment="middle"
+                    on:tap={() => (filter = null)} />
+            </gridlayout>
+        {/if}
+        <collectionView itemTemplateSelector={(item) => item.type || 'default'} items={filteredOptions} row={1} {rowHeight}>
+            <Template key="checkbox" let:item>
+                <checkbox checked={item.value} text={item.name} on:checkedChange={(e) => onCheckBox(item, e.value)} />
+            </Template>
+            <Template let:item>
+                <canvaslabel color={item.color || $textColor} padding={16} rippleColor={item.color || $textColor} on:tap={(event) => onTap(item, event)}>
+                    <cspan fontFamily={mdiFontFamily} fontSize={iconFontSize} text={item.icon} textAlignment="left" verticalAlignment="middle" visibility={item.icon ? 'visible' : 'hidden'} />
+                    <cspan {fontSize} {fontWeight} paddingLeft={item.icon ? 30 : 0} text={item.name} textAlignment="left" verticalAlignment="middle" />
+                    {#if showBorders}
+                        <line color={$borderColor} height={1} startX={0} startY={0} stopX="100%" stopY={0} strokeWidth={1} verticalAlignment="bottom" />
+                    {/if}
+                </canvaslabel>
+            </Template>
+        </collectionView>
+    </gridlayout>
 </gesturerootview>
