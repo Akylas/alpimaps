c9e95d7ec3f6d7affc80f6840244ffeb8d964926
farfromrefug <dev@akylas.fr>
2024-05-02 12:57:47 +0200

    chore: deps update

--- typings/references.d.ts
+++ typings/references.d.ts
@@ -7,26 +7,6 @@
 /// <reference path="../node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!LinkPresentation.d.ts" />
 /// <reference path="../node_modules/@nativescript-community/ui-carto/typings/carto.android.d.ts" />
 
-declare module 'svelte/internal' {
-    export function get_current_component();
-}
-
-// import type { Color } from '@nativescript/core';
-declare module '*.scss' {
-    export const locals;
-}
-
-declare namespace akylas {
-    export namespace alpi {
-        export namespace maps {
-            export class WorkersContext {
-                public static getValue(key): any;
-                public static setValue(key: string, value);
-            }
-        }
-    }
-}
-
 declare const gVars: {
     platform: string;
     CARTO_TOKEN: string;
@@ -68,95 +48,10 @@ declare const TEST_ZIP_STYLES: boolean;
 declare const PLAY_STORE_BUILD: boolean;
 // declare const process: { env: any };
 
-declare namespace akylas {
-    export namespace alpi {
-        export namespace maps {
-            class VectorTileEventListener extends com.akylas.carto.additions.AKVectorTileEventListener {}
-            class BgService extends globalAndroid.app.Service {}
-            class BgServiceBinder extends globalAndroid.os.Binder {}
-            class Utils {
-                static applyDayNight(context: android.content.Context, applyDynamicColors: boolean);
-                static applyDynamicColors(context: android.content.Context);
-                static getDimensionFromInt(context: android.content.Context, intToGet);
-                static getColorFromInt(context: android.content.Context, intToGet);
-                static getColorFromName(context: android.content.Context, intToGet);
-                static restartApp(context: android.content.Context, activity: android.app.Activity);
-                static getSystemLocale(): java.util.Locale;
-            }
-        }
-    }
-}
-
 interface LatLonKeys {
     lat: number;
     lon: number;
     altitude?: number;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unnecessary-qualifier
-declare namespace svelteNative.JSX {
-    type Override<What, With> = Omit<What, keyof With> & With;
-    type ViewKeys = keyof TViewAttributes;
-    type TViewAugmentedAttributes = Override<
-        TViewAttributes,
-        {
-            disableCss?: boolean;
-            rippleColor?: string;
-            sharedTransitionTag?: string;
-            verticalAlignment?: string;
-            dynamicElevationOffset?: string | number;
-            elevation?: string | number;
-        }
-    >;
-    type ViewAndroidAttributes = {
-        [K in keyof TViewAugmentedAttributes as `android:${K}`]: TViewAugmentedAttributes[k];
-    };
-    type ViewIOSAttributes = {
-        [K in keyof TViewAugmentedAttributes as `ios:${K}`]: TViewAugmentedAttributes[k];
-    };
-    type ViewAttributes = TViewAugmentedAttributes & ViewAndroidAttributes & ViewIOSAttributes;
-
-    interface ButtonAttributes {
-        variant?: string;
-        shape?: string;
-    }
-    interface ImageAttributes {
-        noCache?: boolean;
-        imageRotation?: number;
-        colorMatrix?: number[];
-        blurRadius?: number;
-        fadeDuration?: number;
-        'on:rotateAnimated'?: (args: EventData) => void;
-    }
-    interface SpanAttributes {
-        verticalAlignment?: string;
-        verticalTextAlignment?: string;
-    }
-    interface SliderAttributes {
-        stepSize?: number;
-        trackBackgroundColor?: string;
-    }
-    interface PageAttributes {
-        statusBarColor?: string;
-        screenOrientation?: string;
-        keepScreenAwake?: boolean;
-        screenBrightness?: number;
-        'on:closingModally'?: (args: ShownModallyData) => void;
-        // "on:shownModally"?: (args: ShownModallyData) => void;
-    }
-    interface LabelAttributes {
-        autoFontSize?: boolean;
-        verticalTextAlignment?: string;
-        maxLines?: number;
-        minFontSize?: number;
-        maxFontSize?: number;
-        lineBreak?: string;
-        html?: string;
-        linkColor?: string;
-        selectable?: boolean;
-        onlinkTap?;
-        'on:linkTap'?;
-    }
 }
 
 // interface SunlightTimes {
