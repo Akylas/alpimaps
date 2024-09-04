fe25083596a8a650921a1b77e8e06eb7859c5d2c
farfromrefug <dev@akylas.fr>
2024-09-04 15:35:55 +0200

    feat: new map font scale option to scale all labels on map

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,3 +1,7 @@
 import { Screen } from '@nativescript/core';
 
 export const ALERT_OPTION_MAX_HEIGHT = Screen.mainScreen.heightDIPs * 0.47;
+
+export const SETTINGS_MAP_FONT_SCALE = 'map_font_scale';
+
+export const MAP_FONT_SCALE = 1;
