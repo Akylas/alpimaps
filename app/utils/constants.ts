ec24d85d2fa8af7447f2526e1652a86d9ab6c25d
farfromrefug <dev@akylas.fr>
2024-10-27 14:58:04 +0100

    feat: allow customizing valhalla offline routing service

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -8,3 +8,11 @@ export const SETTINGS_COLOR_THEME = 'color_theme';
 export const DEFAULT_COLOR_THEME = 'default';
 
 export const MAP_FONT_SCALE = 1;
+
+export const SETTINGS_VALHALLA_MAX_DISTANCE_PEDESTRIAN = 'service_limits.pedestrian.max_distance';
+export const SETTINGS_VALHALLA_MAX_DISTANCE_BICYCLE = 'service_limits.bicycle.max_distance';
+export const SETTINGS_VALHALLA_MAX_DISTANCE_AUTO = 'service_limits.auto.max_distance';
+
+export const DEFAULT_VALHALLA_MAX_DISTANCE_PEDESTRIAN = 250000;
+export const DEFAULT_VALHALLA_MAX_DISTANCE_BICYCLE = 500000;
+export const DEFAULT_VALHALLA_MAX_DISTANCE_AUTO = 5000000;
