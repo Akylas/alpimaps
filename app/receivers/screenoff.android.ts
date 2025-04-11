98ec4cd2bf4372decc8e43abecef4381295aa488
Martin Guillon <dev@akylas.fr>
2025-04-11 21:46:05 +0200

    chore: new screen off auto refresh featurefor Hisense A9

--- app/receivers/screenoff.android.ts
+++ app/receivers/screenoff.android.ts
@@ -1,6 +1,20 @@
+import { startRefreshAlarm, stopRefreshAlarm } from '~/utils/utils';
+
 @NativeClass
 export class ScreenOnOffReceiver extends android.content.BroadcastReceiver {
-    constructor(args) {
+    constructor(private args) {
         super(args);
+    }
+    onReceive(context: android.content.Context, intent: android.content.Intent) {
+        DEV_LOG && console.log("ScreenOnOffReceiver");
+        const action = intent.getAction();
+            DEV_LOG && console.log('screenOnOffReceiver', action);
+              if (action === android.content.Intent.ACTION_SCREEN_ON) {
+                DEV_LOG && console.log("Screen ON");
+                stopRefreshAlarm();
+              } else if (action === android.content.Intent.ACTION_SCREEN_OFF) {
+                DEV_LOG && console.log("Screen OFF");
+                startRefreshAlarm();
+              }
     }
 }
