1df946972b18d7a183365f58623b8dc665e4daa1
Martin Guillon <dev@akylas.fr>
2025-04-11 19:28:34 +0200

    chore: new screen off auto refresh featurefor Hisense A9

---
+++ app/receivers/screenoff.android.ts
@@ -0,0 +1,6 @@
+@NativeClass
+export class ScreenOnOffReceiver extends android.content.BroadcastReceiver {
+    constructor(args) {
+        super(args);
+    }
+}
---
+++ app/receivers/screenoff.d.ts
@@ -0,0 +1,3 @@
+export class ScreenOnOffReceiver {
+    constructor(args);
+}
