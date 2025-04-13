7384889b070c3e32c74271462a5cea485d6647ec
Martin Guillon <dev@akylas.fr>
2025-04-13 18:34:19 +0200

    chore: build fix

--- app/android/refresh_alarm_receiver.android.ts
+++ app/android/refresh_alarm_receiver.android.ts
@@ -1,6 +1,5 @@
 import { scheduleRefreshAlarm } from '~/utils/utils';
 import { ApplicationSettings } from '@nativescript/core';
-import { getMapContext } from '~/mapModules/MapModule';
 @JavaProxy('__PACKAGE__.RefreshAlarmReceiver')
 @NativeClass
 export class RefreshAlarmReceiver extends android.content.BroadcastReceiver {
@@ -8,15 +7,6 @@ export class RefreshAlarmReceiver extends android.content.BroadcastReceiver {
         DEV_LOG && console.log("AlarmReceiver", "Alarm triggered!")
         
         const broadcastIntent = new android.content.Intent(ApplicationSettings.getString('refreshAlarmBroadcast', "com.akylas.A9_REFRESH_SCREEN"));       
-        const module = getMapContext().mapModule('userLocation');
-        // we are waiting for a location update
-        if (module) {
-          broadcastIntent.set extra('sleep_delay', 0);
-          module.once('location', ()=> {
-              //1context.sendBroadcast(broadcastIntent);
-          })
-        } else {}
-                
         context.sendBroadcast(broadcastIntent);
                 
        // scheduleRefreshAlarm();
