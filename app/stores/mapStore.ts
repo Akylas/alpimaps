9bb86462253b0d774bc1bee4ee62f056c8160903
Martin Guillon <dev@akylas.fr>
2025-06-20 17:44:54 +0200

    chore: testing route features

--- app/stores/mapStore.ts
+++ app/stores/mapStore.ts
@@ -364,7 +364,7 @@ function createStore(params){
             } else {
                 updateMethod(key, value);
             }
-            notifyCallback?.({eventName:'change', object:nutiProps, key, value, nutiValue: nutiTransform ? nutiTransform(value) : value + ''});
+            notifyCallback?.({eventName:'change', object:nutiProps, ...obj, nutiValue: nutiTransform ? nutiTransform(value) : value + ''});
         });
         obj.updateMethod = updateMethod;
         
@@ -387,7 +387,7 @@ function createStore(params){
               } else {
                   obj.updateMethod(settingKey, value);
               }
-              notifyCallback?.({eventName:'change', object:this, key, value, nutiValue: nutiTransform ? nutiTransform(value) : value + ''});
+              notifyCallback?.({eventName:'change', object:this, ...obj, nutiValue: nutiTransform ? nutiTransform(value) : value + ''});
           } catch (error) {
               showError(error)
           }
