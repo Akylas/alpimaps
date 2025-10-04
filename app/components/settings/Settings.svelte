16457e147e30e5f9026042e4010a03122029f521
farfromrefug <dev@akylas.fr>
2025-10-04 19:16:40 +0200

    chore: status bar color

--- app/components/settings/Settings.svelte
+++ app/components/settings/Settings.svelte
@@ -1213,7 +1213,7 @@
     onMapLanguageChanged(refresh);
 </script>
 
-<page actionBarHidden={true}>
+<page actionBarHidden={true} android:statusBarColor={colorPrimary}>
     <gridlayout paddingLeft={$windowInset.left} paddingRight={$windowInset.right} rows="auto,*">
         <collectionview bind:this={collectionView} itemTemplateSelector={selectTemplate} {items} row={1} android:paddingBottom={windowInsetBottom + $windowInset.keyboard}>
             <Template key="sectionheader" let:item>
