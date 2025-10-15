402be8fa83133092319faffc7102d8f4c3678b65
farfromrefug <dev@akylas.fr>
2025-10-15 09:40:21 +0200

    chore: regression fix

--- app/components/settings/SettingsSwitch.svelte
+++ app/components/settings/SettingsSwitch.svelte
@@ -1,21 +1,16 @@
 <svelte:options accessors />
 
 <script context="module" lang="ts">
-    import { colors } from '~/variables';
     import ListItemAutoSize from '~/components/common/ListItemAutoSize.svelte';
 </script>
 
 <script lang="ts">
-    $: ({ colorOutlineVariant } = $colors);
-
     export let item;
-    export let onCheckBox: (item, value) => void = null;
-
-    $: DEV_LOG && console.log('SettingsSwitch', JSON.stringify(item));
+    export let onCheckBox: (item, value, e) => void = null;
 
     function onCheckChanged(e) {
         item.value = e.value;
-        onCheckBox?.(item, e.value);
+        onCheckBox?.(item, e.value, e);
     }
 </script>
 
