a6d8d0deb2f847d1a60e908a746c6cc14ad2bfa0
Martin Guillon <dev@akylas.fr>
2026-03-15 16:50:31 +0100

    chore: sub

--- typings/references.d.ts
+++ typings/references.d.ts
@@ -1,11 +1,4 @@
-/// <reference path="../node_modules/@nativescript/core/global-types.d.ts" />
-/// <reference path="../node_modules/@nativescript/types-android/lib/android-32.d.ts" />
-/// <reference path="../node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!Foundation.d.ts" />
-/// <reference path="../node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!ObjectiveC.d.ts" />
-/// <reference path="../node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!UIKit.d.ts" />
-/// <reference path="../node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!CoreLocation.d.ts" />
-/// <reference path="../node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!LinkPresentation.d.ts" />
-/// <reference path="../node_modules/@nativescript/types-ios/lib/ios/interop.d.ts" />
+
 /// <reference path="../node_modules/@nativescript-community/ui-carto/typings/carto.android.d.ts" />
 
 declare const gVars: {
