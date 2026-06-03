5dab6f47ec6407b04761d7ba23a9ac17c35d37fa
farfromrefuge <dev@akylas.fr>
2026-06-03 15:48:27 +0200

    chore: ios build fix

--- nativescript.config.js
+++ nativescript.config.js
@@ -26,6 +26,12 @@ module.exports = {
                 libs: ['CartoMobileSDKCore'],
                 repositoryURL: 'https://github.com/Akylas/mobile-sdk-ios-swift.git',
                 version: '5.2.3'
+            },
+            {
+                name: 'carto-SwiftTryCatch-sdk',
+                libs: ['SwiftTryCatch'],
+                repositoryURL: 'https://github.com/farfromrefug/SwiftTryCatch.git',
+                version: '1.0.0'
             }
         ]
     },
